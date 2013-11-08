686a77aa4dd3826235a03485f05eea1a8dc1ada3
Aaron Froehlich <aaronfroehlich@gmail.com>
2013-11-08 09:08:45 -0500

    Adds certs and Cornell service provider

--- simplesamlphp-1.10.0/config/authsources.php
+++ simplesamlphp-1.10.0/config/authsources.php
@@ -13,22 +13,28 @@ $config = array(
 
 	// An authentication source which can authenticate against both SAML 2.0
 	// and Shibboleth 1.3 IdPs.
-	'default-sp' => array(
-		'saml:SP',
+	// 'default-sp' => array(
+	// 	'saml:SP',
 
-		// The entity ID of this SP.
-		// Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
-		'entityID' => NULL,
+	// 	// The entity ID of this SP.
+	// 	// Can be NULL/unset, in which case an entity ID is generated based on the metadata URL.
+	// 	'entityID' => NULL,
 
-		// The entity ID of the IdP this should SP should contact.
-		// Can be NULL/unset, in which case the user will be shown a list of available IdPs.
-		'idp' => NULL,
+	// 	// The entity ID of the IdP this should SP should contact.
+	// 	// Can be NULL/unset, in which case the user will be shown a list of available IdPs.
+	// 	'idp' => NULL,
 
-		// The URL to the discovery service.
-		// Can be NULL/unset, in which case a builtin discovery service will be used.
-		'discoURL' => NULL,
-	),
+	// 	// The URL to the discovery service.
+	// 	// Can be NULL/unset, in which case a builtin discovery service will be used.
+	// 	'discoURL' => NULL,
+	// ),
 
+  'cornell' => array(
+    'saml:SP',
+    'privatekey' => 'saml.pem',
+    'certificate' => 'saml.crt',
+    'idp' => 'https://shibidp.cit.cornell.edu/idp/shibboleth',
+  ),
 
 	/*
 	'example-sql' => array(
