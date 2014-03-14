5fe7b35827c2297f56da9dc9e4e2512869ccbda2
Caroline Hecht <cgh2@cornell.edu>
2014-03-14 14:49:03 -0400

    Adds template files for 5 forms and updates the processing in forms.module to use them

---
+++ docroot/sites/all/modules/features/forms/templates/career-services-contact-form.tpl.php
@@ -0,0 +1,13 @@
+<?php print $salutation;?>
+
+Thank you for contacting ILR Career Services.
+
+Your questions/comments were submitted on <? print $submission_date; ?>.
+
+<? print $form_data;?>
+
+You will receive a response within two business days.
+
+Sincerely,
+<? print $org; ?> / <? print $admin_email; ?>
+Office of Career Services, ILR School, Cornell University
