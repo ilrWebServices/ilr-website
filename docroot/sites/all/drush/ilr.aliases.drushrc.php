991a5e3ff48620f6599e11e152f91bdf6df61f1b
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-03-26 11:10:08 -0400

    Updates drush aliases to include Acquia test

--- docroot/sites/all/drush/ilr.aliases.drushrc.php
+++ docroot/sites/all/drush/ilr.aliases.drushrc.php
@@ -16,6 +16,22 @@ $aliases['ilr.dev'] = array(
   ),
 );
 
+// ILR site, test env
+// Access with @ilr.ilr.test
+$aliases['ilr.test'] = array(
+  'parent' => '@parent',
+  'site' => 'ilr',
+  'env' => 'test',
+  'uri' => 'http://stage.ilr.cornell.edu',
+  'root' => '/var/www/html/ilr.test/docroot',
+  'remote-host' => 'srv-2136.devcloud.hosting.acquia.com',
+  'remote-user' => 'ilr',
+  'path-aliases' => array (
+    '%dump-dir' => '/mnt/files/ilrtest/import',
+    '%files' => '/var/www/html/ilr.test/docroot/sites/stage.ilr.cornell.edu/files',
+  ),
+);
+
 // Local
 $aliases['ilr.local'] = array(
   'parent' => '@parent',
