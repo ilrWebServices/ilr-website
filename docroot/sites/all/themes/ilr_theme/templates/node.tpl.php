302b574bccf2bbb7b8995d9272f6ae11fce478d6
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-08-01 15:38:26 -0400

    Modifies node and block templates

--- docroot/sites/all/themes/ilr_theme/templates/node.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/node.tpl.php
@@ -14,14 +14,12 @@
     </div>
   <?php endif; ?>
 
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($content['comments']);
-      hide($content['links']);
-      print render($content);
-    ?>
-  </div>
+  <?php
+    // We hide the comments and links now so that we can render them later.
+    hide($content['comments']);
+    hide($content['links']);
+    print render($content);
+  ?>
 
   <?php print render($content['links']); ?>
   <?php print render($content['comments']); ?>
