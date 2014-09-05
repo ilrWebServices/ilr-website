765c4a997bf6e88ecf8756719cb86ccdf395aa74
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-09-05 16:52:11 -0400

    Makes CU emergency banners protocol agnostic

--- docroot/sites/all/themes/ilr_theme/template.php
+++ docroot/sites/all/themes/ilr_theme/template.php
@@ -13,7 +13,6 @@ function ilr_theme_preprocess_html(&$variables) {
   // Footer
   drupal_add_js('try{Typekit.load();}catch(e){}', array('type' => 'inline', 'scope' => 'footer'));
   drupal_add_js($variables['directory'] . '/js/vendor/eq.min.js', array('type' => 'file', 'scope' => 'footer'));
-  drupal_add_js('https://embanner.univcomm.cornell.edu/OWC-emergency-banner.js', array('type' => 'external', 'scope' => 'footer'));
 }
 
 /**
