3035a647906451bd46dabf534dd2514476fcfcb3
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-09-11 09:28:25 -0400

    Updates page template html and makes header fixed in position above jpanel container

--- docroot/sites/all/modules/custom/jpanelmenu/js/jpanelmenu.js
+++ docroot/sites/all/modules/custom/jpanelmenu/js/jpanelmenu.js
@@ -5,23 +5,24 @@
  */
 
 (function($){
-	$(document).ready(function(){
-		var jPM = $.jPanelMenu({
-    		menu: Drupal.settings.jpanelmenu.target,
-    		trigger: Drupal.settings.jpanelmenu.trigger,
-    		direction: Drupal.settings.jpanelmenu.direction,
-    		openPosition: Drupal.settings.jpanelmenu.openPosition,
-		});
+  $(document).ready(function(){
+    var jPM = $.jPanelMenu({
+      menu: Drupal.settings.jpanelmenu.target,
+      trigger: Drupal.settings.jpanelmenu.trigger,
+      direction: Drupal.settings.jpanelmenu.direction,
+      openPosition: Drupal.settings.jpanelmenu.openPosition,
+      excludedPanelContent: 'header',
+    });
 
     // If the jPanelMenu is showing in a narrow window on  desktop browser and the user
     // widens their window beyond the maximum visibility breakpoint for jPanel, hide the jPanelMenu
     $(window).on("resize", function() {
-        if ($('#page-wrapper[data-eq-state]').attr('data-eq-state') == 'regular-nav') {
-            jPM.close();
-        }
+      if ($('header').attr('data-eq-state') == 'regular-nav') {
+        jPM.close();
+      }
     });
 
     // Turn on the jPanelMenu
     jPM.on();
-	});
+  });
 })(jQuery);
--- docroot/sites/all/themes/ilr_theme/templates/page.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/page.tpl.php
@@ -1,96 +1,88 @@
-<div id="page-wrapper"  <?php print render($page['nav_trigger_pts']); ?>>
-  <div id="bg"><div class="search-bg"></div></div>
-  <div id="page">
-    <div class="container">
-      <header role="banner" <?php print render($page['page_width_eq_points']); ?>>
-
-          <div class="logo-wrapper">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-              <?php print '<img src="/' . drupal_get_path('theme', 'ilr_theme') . '/images/logo.svg" alt="Cornell University | ILR School" title="Cornell University | ILR School">';?>
-            </a>
-          </div>
-          <div id="search-button">
-            <a href="#">
-              <?php print '<img src="/' . drupal_get_path('theme', 'ilr_theme') . '/images/search.svg" alt="search" title="search">';?>
-            </a>
-          </div>
-          <div class="jpanel-trigger-container"><a href="#" class="jpanel-trigger">MENU</a></div>
-          <div id="search-form">
-            <form action="/search" method="get" id="cu-search-form" accept-charset="UTF-8">
-              <input id="search-form-query" type="text" name="s" placeholder="Search ILR . . ." value="" size="20" maxlength="128" class="form-text" />
-            </form>
-          </div>
-          <?php if ($main_menu): ?>
-          <nav role="navigation" class="main">
-            <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
-          </nav> <!-- / nav -->
-          <?php endif; ?>
-          <?php print render($page['header']); ?>
-      </header> <!-- / header -->
-
-      <?php print $messages; ?>
-
-      <!-- highlighted location -->
-      <!-- Add subsite nav region here -->
-
-      <div id="main" role="main" <?php print render($page['page_width_eq_points']); ?>>
-
-        <?php if ($page['highlighted']): ?>
-        <div id="highlighted">
-          <div class="section">
-          <?php print render($page['highlighted']); ?>
-          </div>
-        </div><?php endif; ?>
-        <div id="content" class="column">
-          <?php if ($breadcrumb): ?>
-          <div id="breadcrumb"><?php print $breadcrumb; ?></div>
-          <?php endif; ?>
-          <div class="section">
-            <a id="main-content"></a>
-            <?php print render($title_prefix); ?>
-            <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-            <?php print render($title_suffix); ?>
-            <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-            <?php print render($page['help']); ?>
-            <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
-            <?php print render($page['content']); ?>
-            <?php print $feed_icons; ?>
-          </div>
-        </div> <!-- /.section, /#content -->
-
-        <div id="sidebar-first" class="column sidebar">
-          <div class="section">
-            <div class="main-nav">
-              <?php print render($variables['main_menu_expanded']); ?>
-            </div>
-        </div></div> <!-- /.section, /#sidebar-first -->
-
-        <?php if ($page['sidebar_second']): ?>
-          <div id="sidebar-second" class="column sidebar">
-            <div class="section">
-            <?php print render($page['sidebar_second']); ?>
-            </div>
-          </div> <!-- /.section, /#sidebar-second -->
+<header role="banner" <?php print render($page['nav_trigger_pts']); ?>>
+  <div class="container">
+    <div class="logo-wrapper">
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <?php print '<img src="/' . drupal_get_path('theme', 'ilr_theme') . '/images/logo.svg" alt="Cornell University | ILR School" title="Cornell University | ILR School">';?>
+      </a>
+    </div>
+    <div id="search-button">
+      <a href="#">
+        <?php print '<img src="/' . drupal_get_path('theme', 'ilr_theme') . '/images/search.svg" alt="search" title="search">';?>
+      </a>
+    </div>
+    <div class="jpanel-trigger-container"><a href="#" class="jpanel-trigger">MENU</a></div>
+    <div id="search-form">
+      <form action="/search" method="get" id="cu-search-form" accept-charset="UTF-8">
+        <input id="search-form-query" type="text" name="s" placeholder="Search ILR . . ." value="" size="20" maxlength="128" class="form-text" />
+      </form>
+    </div>
+    <?php if ($main_menu): ?>
+    <nav role="navigation" class="main">
+      <?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Main menu'))); ?>
+    </nav> <!-- / nav -->
+    <?php endif; ?>
+    <?php print render($page['header']); ?>
+  </div>
+</header>
+<div id="page">
+  <div class="container" <?php print render($page['nav_trigger_pts']); ?>>
+    <?php print $messages; ?>
+    <div id="main" role="main" <?php print render($page['page_width_eq_points']); ?>>
+      <?php if ($page['highlighted']): ?>
+      <div id="highlighted">
+        <div class="section">
+        <?php print render($page['highlighted']); ?>
+        </div>
+      </div><?php endif; ?>
+      <div id="content" class="column">
+        <?php if ($breadcrumb): ?>
+        <div id="breadcrumb"><?php print $breadcrumb; ?></div>
         <?php endif; ?>
+        <div class="section">
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+          <?php print render($page['help']); ?>
+          <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+          <?php print render($page['content']); ?>
+          <?php print $feed_icons; ?>
+        </div>
+      </div> <!-- /.section, /#content -->
 
-        <?php if ($page['content_bottom']): ?>
-          <div id="content-bottom" <?php print render($page['page_width_eq_points']); ?>>
-            <div class="section">
-            <?php print render($page['content_bottom']); ?>
-            </div>
-          </div> <!-- /.section, /#content-bottom -->
-        <?php endif; ?>
-      </div> <!-- /#main -->
-    </div><!-- /.container -->
-    <footer role="contentinfo">
-      <div class="section">
-        <?php if ($secondary_menu): ?>
-          <nav role="navigation" class="secondary">
-            <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
-          </nav> <!-- / nav -->
-        <?php endif; ?>
-        <?php print render($page['footer']); ?>
-      </div>  <!-- /.section -->
-    </footer> <!-- / footer -->
-  </div><!-- /#page -->
-</div> <!-- /#page-wrapper -->
+      <div id="sidebar-first" class="column sidebar">
+        <div class="section">
+          <div class="main-nav">
+            <?php print render($variables['main_menu_expanded']); ?>
+          </div>
+      </div></div> <!-- /.section, /#sidebar-first -->
+
+      <?php if ($page['sidebar_second']): ?>
+        <div id="sidebar-second" class="column sidebar">
+          <div class="section">
+          <?php print render($page['sidebar_second']); ?>
+          </div>
+        </div> <!-- /.section, /#sidebar-second -->
+      <?php endif; ?>
+
+      <?php if ($page['content_bottom']): ?>
+        <div id="content-bottom" <?php print render($page['page_width_eq_points']); ?>>
+          <div class="section">
+          <?php print render($page['content_bottom']); ?>
+          </div>
+        </div> <!-- /.section, /#content-bottom -->
+      <?php endif; ?>
+    </div> <!-- /#main -->
+  </div><!-- /.container -->
+</div><!-- /#page -->
+<footer role="contentinfo">
+  <div class="container">
+    <?php if ($secondary_menu): ?>
+      <nav role="navigation" class="secondary">
+        <?php print theme('links__system_secondary_menu', array('links' => $secondary_menu, 'attributes' => array('id' => 'secondary-menu', 'class' => array('links', 'inline', 'clearfix')), 'heading' => t('Secondary menu'))); ?>
+      </nav> <!-- / nav -->
+    <?php endif; ?>
+    <?php print render($page['footer']); ?>
+  </div>  <!-- /.container -->
+</footer> <!-- / footer -->
