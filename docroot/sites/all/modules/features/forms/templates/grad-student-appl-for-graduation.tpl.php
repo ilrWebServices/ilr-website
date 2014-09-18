c6dd9715f53db486c1b83fd39c192d793928e874
Caroline Hecht <cgh2@cornell.edu>
2014-09-18 14:46:24 -0400

    Adds tpl.php files for Grad and Undergrad Application to Graduate for user email

---
+++ docroot/sites/all/modules/features/forms/templates/grad-student-appl-for-graduation.tpl.php
@@ -0,0 +1,13 @@
+<?php print $salutation;?>
+
+This is an automated confirmation of your ILR Graduate Student Application for Graduation.
+
+Please stop by the ILR Graduate Programs Office to pick up your commencement packet. These packets contain severe weather tickets and other important information. For additional information about commencement please visit http://commencement.cornell.edu/.
+
+If you have any questions or concerns about your application for graduation, please
+email the ILR Graduate Programs Office at mjk53@cornell.edu.
+
+Sincerely,
+ILR Graduate Programs Office
+
+<? print $form_data;?>
