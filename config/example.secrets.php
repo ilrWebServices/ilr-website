84d5f76766df5678e00f6b0bdedd39422b8c0b74
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-10-09 15:50:15 -0400

    Adds note about uploading secret file

--- config/example.secrets.php
+++ config/example.secrets.php
@@ -1,6 +1,9 @@
 <?php
 /**
  * Configuration file containing sensitive data needed by the application
+ * **NOTE**
+ * You will need to manually upload the secrets.php file to the config folder on Acquia
+ * since it does not get deployed with the site.
  */
 
 $secret = 'super duper secret';
