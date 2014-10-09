2cfd1a365aaf746de1cfde80ba770e3ede7d3a14
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-10-09 15:46:35 -0400

    Adds secrets file to forms module

---
+++ config/env.php
@@ -0,0 +1,9 @@
+<?php
+
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+  // We're on Acquia
+  define('CONFIG_PATH', '/mnt/gfs/home/ilr/config/');
+} // We're developing locally
+else {
+  define('CONFIG_PATH', DRUPAL_ROOT . '/../config/');
+}
---
+++ config/example.secrets.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * Configuration file containing sensitive data needed by the application
+ */
+
+$secret = 'super duper secret';
