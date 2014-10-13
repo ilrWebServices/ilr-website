92d95dc6884d025d76598bce3d2d575563210545
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-10-13 15:09:00 -0400

    Adds js for mega menu

---
+++ docroot/sites/all/modules/features/ilr_mega_menu/ilr_mega_menu.js
@@ -0,0 +1,46 @@
+(function ($) {
+  Drupal.behaviors.ilr_mega_menu = {
+    attach: function (context, settings) {
+      $('#block-ilr-mega-menu-ilr-mega-menu li.menu-item').mouseenter(function(){
+        $('#block-ilr-mega-menu-ilr-mega-menu').addClass('active');
+        submenu = $(this).find('div.submenu');
+        if ($('.submenu.active').length) {
+          $('.submenu.active').removeClass('active').hide();
+          $(submenu).show();
+        } else {
+          $(submenu).slideDown();
+        }
+        $(submenu).addClass('active');
+        $('#block-ilr-mega-menu-ilr-mega-menu').mouseleave(function() {
+          $(submenu).slideUp();
+          $('#block-ilr-mega-menu-ilr-mega-menu').removeClass('active');
+          $(submenu).removeClass('active');
+        });
+      });
+
+      // Edit page
+      if (typeof Drupal.settings.ilr_mega_menu !== 'undefined') {
+        current = $('#edit-field-menu-name-und').val();
+        setTopLevelMenuOptions(current);
+        $('#edit-field-menu-name-und').change(function() {
+          selected = $(this).val();
+          setTopLevelMenuOptions(selected);
+        });
+      }
+
+      function setTopLevelMenuOptions(menuName) {
+        options = Drupal.settings.ilr_mega_menu.topLevelItems[menuName];
+        var $el = $("#edit-field-mega-menu-item-und");
+        $el.empty(); // remove old options
+        $.each(options, function(key, value) {
+          $el.append($("<option>"+value+"</option>")
+             .attr("value", key));
+        });
+
+        if (Drupal.settings.ilr_mega_menu.currentItem.length) {
+          $("#edit-field-mega-menu-item-und").val(Drupal.settings.ilr_mega_menu.currentItem);
+        }
+      }
+    }
+  };
+}(jQuery));
