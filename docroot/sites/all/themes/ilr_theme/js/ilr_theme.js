286fec1c06a4799f342a92e0bc67974b3b6521c6
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-10-29 16:10:25 -0400

    Removes ssl link for google translate

--- docroot/sites/all/themes/ilr_theme/js/ilr_theme.js
+++ docroot/sites/all/themes/ilr_theme/js/ilr_theme.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.ilr_theme = {
     attach: function (context, settings) {
-      currentURL = location.href;
+      currentURL = location.href.replace('https','http');
       $('#google-translation').append('<input name="u" value="'+currentURL+'" type="hidden" />');
       $('#google-translation input').click(function(){
         $('#google-translation input[name="langpair"]').val($(this).val());
