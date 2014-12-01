a61236f0c250a06114e9c75ccfb215b1f6499f25
Caroline Hecht <cgh2@cornell.edu>
2014-12-01 15:01:52 -0500

    Adds WIP code for total cost in emails for alumni event registration

---
+++ docroot/sites/all/modules/features/forms/templates/ilr-alumni-association-event-reg.tpl.php
@@ -0,0 +1,17 @@
+<?php print $salutation;?>
+
+Your online reservation for the following ILR Alumni Association event has been received:
+
+Event details:
+
+<? print $form_title; ?>
+
+<? print $event_info_for_email; ?>
+
+Your registration information is shown below.  Please review this information and contact Sue Sappington in the ILR Alumni Affairs Development Office, ss446@cornell.edu, if you have any questions about your registration.
+
+<? print $event_total_cost_for_email;?>
+
+<? print $form_data;?>
+
+Thank you!
