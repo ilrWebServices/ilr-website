15fca33191beb00addb809ed1c396d4a0563ef13
Caroline Hecht <cgh2@cornell.edu>
2014-12-05 15:40:05 -0500

    Adds three NLLI and ULI tpl.php files

---
+++ docroot/sites/all/modules/features/forms/templates/nlli-sponsoring-union-nomination.tpl.php
@@ -0,0 +1,19 @@
+<?php print $salutation;?>
+
+This is an automated confirmation from the ILR School at Cornell University of your form submission:
+
+<? print $form_title; ?>
+
+Thank you for sponsoring a nominee for the National Labor Leadership Initiative. Each nominee must complete the online Participant Application form at:
+http://www.ilr.cornell.edu/workerinstitute/national-labor-leadership-initiative/NLLIparticipantApplication.html
+
+<? print $form_data;?>
+
+You may contact us at <? print $admin_email; ?>.
+
+Sincerely
+Sherrie Morales
+National Labor Leadership Iniative
+
+--
+This is an automated e-mail from <? print $sitename; ?>.
