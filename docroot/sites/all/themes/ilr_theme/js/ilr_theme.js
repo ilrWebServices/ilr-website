d8b2d91d721d99311500e10eb16917f3be08c3c1
Ted Bowman <ted.git@tedbow.com>
2014-12-12 08:57:13 -0500

    Added View block to show WIT articles

--- docroot/sites/all/themes/ilr_theme/js/ilr_theme.js
+++ docroot/sites/all/themes/ilr_theme/js/ilr_theme.js
@@ -1,10 +1,20 @@
 (function ($) {
-  Drupal.behaviors.ilr_theme = {
+  Drupal.behaviors.ilr_theme_google_trans = {
     attach: function (context, settings) {
       currentURL = location.href.replace('https','http');
       $('#google-translation').append('<input name="u" value="'+currentURL+'" type="hidden" />');
       $('#google-translation input').click(function(){
         $('#google-translation input[name="langpair"]').val($(this).val());
+      });
+    }
+  };
+  Drupal.behaviors.ilr_theme_collapse_list = {
+    attach: function (context, settings) {
+      var title_links = $('.collapse-list article h2');
+      title_links.siblings().slideToggle();
+      title_links.click(function(e){
+        e.preventDefault();
+        $(this).siblings().slideToggle(200);
       });
 
     }
