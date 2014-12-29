936ea3302b1df15867cbea346f459c0af044d9df
Aaron Froehlich <aaronfroehlich@gmail.com>
2014-12-29 16:04:24 -0500

    Adds additional toggleable fields for mobile profiles

--- docroot/sites/all/modules/features/people_profiles/people_profiles.js
+++ docroot/sites/all/modules/features/people_profiles/people_profiles.js
@@ -4,12 +4,14 @@
       if ($('article.profile-type-faculty').length) {
         $(window).load(function() {
           if (parseInt($('#main').attr('data-eq-state')) < 768) {
-            $('.group-main-content .field').slice(1).each(function(){
+            // All except the first
+            $('.group-main-content .field, .group-sidebar .field').slice(1).each(function(){
               $(this).toggle();
               $(this).next('.readmore-js-toggle').hide();
             });
 
-            $('.field-label').slice(1).each(function(){
+            // All except the first
+            $('.field-label, .group-contact-info > h3').slice(1).each(function(){
               $(this).addClass('toggleable');
               $(this).click(function(){
                 $(this).toggleClass('toggled');
