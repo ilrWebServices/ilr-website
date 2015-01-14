f0b7d238710261a0df92f98616195f8a125e8abe
Aaron Froehlich <aaronfroehlich@gmail.com>
2015-01-14 10:17:44 -0500

    Removes temporary stage > prod site def

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -2,9 +2,6 @@
 // Additional production domains
 $sites['ilr.devcloud.acquia-sites.com'] = 'ilr.cornell.edu';
 $sites['www.ilr.cornell.edu'] = 'ilr.cornell.edu';
-// temporarily point staging to prod
-$sites['stage.ilr.cornell.edu'] = 'ilr.cornell.edu';
 
 // Additional staging domain
 $sites['ilrtest.devcloud.acquia-sites.com'] = 'stage.ilr.cornell.edu';
-
