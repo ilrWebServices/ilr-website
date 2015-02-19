f220ebdc0aa1e9cd85b807cccbbae4d866515724
Caroline Hecht <cgh2@cornell.edu>
2015-02-19 15:53:12 -0500

    Adds email tpl.php file for Scheinman webcast registration

---
+++ docroot/sites/all/modules/features/forms/templates/scheinman-webcast-registration.tpl.php
@@ -0,0 +1,31 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the following Scheinman Institute webcast.<br><br>
+
+Event details:<br><br>
+
+<? print $form_title; ?><br><br>
+
+<? print $event_info_for_email; ?><br><br>
+
+The webcast connection information will be sent to you at this email address closer to the event date.<br><br>
+
+Technology Requirements<br><br>
+
+Scheinman Institute webcasts can be viewed using computers or mobile devices. The Silverlight plug-in is required. Click the test link below to test your computer; if necessary, you may be instructed to download Silverlight.<br><br>
+
+If you can watch this test webcast you should have no problem viewing the live event:<br>
+
+http://events.mediasite.com/Mediasite/Play/6d6161cbf54a4e6fbcdcef116d0a2acc1d<br><br>
+
+You should be able to click through the slides on the left, and view the video in the lower right.<br><br>
+
+If you have any questions about the webcast, please let us know.<br><br>
+
+Katrina Nobles<br>
+Cornell University, ILR School<br>
+Email: kn64@cornell.edu<br>
+Phone: (607) 255-0704<br><br>
+
+Your information:<br><br>
+<? print $form_data;?>
