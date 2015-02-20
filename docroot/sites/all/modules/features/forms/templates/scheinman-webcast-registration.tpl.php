0700eafeb669636a022036a1824dc54dcf3d518f
Caroline Hecht <cgh2@cornell.edu>
2015-02-20 16:54:57 -0500

    Updates Scheinman webcast reg email tpl file

--- docroot/sites/all/modules/features/forms/templates/scheinman-webcast-registration.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/scheinman-webcast-registration.tpl.php
@@ -10,7 +10,7 @@ Event details:<br><br>
 
 The webcast connection information will be sent to you at this email address closer to the event date.<br><br>
 
-Technology Requirements<br><br>
+<strong>Technology Requirements</strong><br><br>
 
 Scheinman Institute webcasts can be viewed using computers or mobile devices. The Silverlight plug-in is required. Click the test link below to test your computer; if necessary, you may be instructed to download Silverlight.<br><br>
 
@@ -20,7 +20,45 @@ http://events.mediasite.com/Mediasite/Play/6d6161cbf54a4e6fbcdcef116d0a2acc1d<br
 
 You should be able to click through the slides on the left, and view the video in the lower right.<br><br>
 
-If you have any questions about the webcast, please let us know.<br><br>
+<strong>Additional Information</strong> -
+<a href="http://www.ilr.cornell.edu/scheinman-institute/professional-development/becoming-neutral">
+http://www.ilr.cornell.edu/scheinman-institute/professional-development/becoming-neutral</a>
+<br><br>
+
+Relating to this webcast, there are a variety of channels and resources where you can share your
+experiences and thoughts, accessible from the
+<a href="http://www.ilr.cornell.edu/scheinman-institute/professional-development/becoming-neutral">
+Becoming a Neutral</a> section of the Scheinman Institute web site.<br><br>
+
+Discussion Forum - opens Wednesday, March 4th<br><br>
+
+The Discussion Forum will host several conversation threads with a focus on the neutral profession
+including tips and stories from successful neutrals about how they began their careers.
+It will open on March 4th and close March 18th.<br><br>
+
+Webcast - Wednesday, March 11th, 12:00 - 1:00 pm<br><br>
+
+Based on the topics raised in the Discussion Forum, we will address key questions and concerns in more
+depth during the webcast. We will also be taking live questions and feedback from attendees.<br><br>
+
+Resources - open now!<br><br>
+
+In the Resources section, you can find information on how to become a neutral; how some successful
+professionals got started, obtain a list of professional organizations, and access literature on the
+field and its current status.<br><br>
+
+We hope that participating in these experiences will:
+<ul>
+<li>Provide you with resources to help advance your career</li>
+<li>Establish connections with others so you can continue to build your network</li>
+<li>Offer a safe environment where you can express your needs, concerns and ideas about your
+career and the field itself</li>
+</ul>
+
+Looking forward to the event in March!<br><br>
+
+Best regards,<br>
+The Faculty and Staff of Scheinman Institute on Conflict Resolution<br><br>
 
 Katrina Nobles<br>
 Cornell University, ILR School<br>
