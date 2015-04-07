e34c80a394332118c00a24bf93eb68b478b32e9f
Aaron Froehlich <aaronfroehlich@gmail.com>
2015-04-07 15:54:08 -0400

    Adds initial ilr_at_70 custom module with dependencies

---
+++ docroot/sites/all/modules/custom/ilr_at_70/ilr_at_70.js
@@ -0,0 +1,7 @@
+(function ($) {
+  Drupal.behaviors.ilr_at_70 = {
+    attach: function (context, settings) {
+      console.log('placeholder for custom js events in ilr_at_70');
+    }
+  };
+}(jQuery));
