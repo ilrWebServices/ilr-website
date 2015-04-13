17fe47ea882ec1ce8534136f343e7e49378d4ae4
Aaron Froehlich <aaronfroehlich@gmail.com>
2015-04-13 10:38:37 -0400

    Adds social sharing links

--- docroot/sites/all/modules/custom/ilr_at_70/ilr_at_70.js
+++ docroot/sites/all/modules/custom/ilr_at_70/ilr_at_70.js
@@ -1,7 +1,41 @@
 (function ($) {
   Drupal.behaviors.ilr_at_70 = {
     attach: function (context, settings) {
-      console.log('placeholder for custom js events in ilr_at_70');
+      var $channel, $article, $title, $summary, $id, link;
+      var shareWith = [];
+      // Todo: Add modal click functionality
+      // Prepare share pattern for each channel
+
+      shareWith['twitter'] = function () {
+      };
+
+      shareWith['facebook'] = function () {
+      };
+
+      shareWith['linkedin'] = function () {
+      };
+
+      shareWith['instagram'] = function () {
+      };
+
+      shareWith['email'] = function () {
+      };
+
+      $('.node-reflection .social a').click(function() {
+        $channel = $(this).attr('class');
+        $article = $(this).closest('article');
+        $title = $article.find('h2').text();
+        $summary = $article.find('.field-type-text-with-summary').text();
+        $id = $article.attr('id');
+        var link = window.location.href + '#' + $id;
+        shareWith[$channel]();
+        return false;
+      });
+
+      // @todo: Replace with modal trigger
+      $('.node-reflection a').click(function() {
+        return false;
+      });
     }
   };
 }(jQuery));
