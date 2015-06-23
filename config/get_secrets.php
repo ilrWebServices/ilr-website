8bcad38a236bb70d9fa1508cc2d0202e75499b06
Aaron Froehlich <aaronfroehlich@gmail.com>
2015-06-23 09:31:42 -0400

    Fixes secrets path issue for featuretest

--- config/get_secrets.php
+++ config/get_secrets.php
@@ -3,6 +3,9 @@
 if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
   // We're on Acquia
   define('CONFIG_PATH', '/mnt/gfs/home/ilr/config');
+} // Check for featuretest
+else if (strpos($_SERVER['SERVER_NAME'], 'featuretest.org') > 0) {
+  define('CONFIG_PATH', '/var/aegir/projects/ilr/prod/config');
 } // We're developing locally
 else {
   define('CONFIG_PATH', dirname(__FILE__));
