e6a0497f1ffa0f0305998e3614f80d229bf0c8a3
Aaron Froehlich <aaronfroehlich@gmail.com>
2015-06-30 13:58:04 -0400

    Configures readmorejs for faculty bios on classes

--- docroot/sites/all/modules/features/ilr_sdc_listings/js/ilr_sdc_listings.js
+++ docroot/sites/all/modules/features/ilr_sdc_listings/js/ilr_sdc_listings.js
@@ -18,15 +18,16 @@
         $('div.class-'+$classId).show();
       });
 
-      // What is this?
-      // $(".node-teaser").click(function(){
-      //   window.location=$(this).find("a").attr("href"); return false;
-      // });
-
       // Show the first class detail toggle div
       $(window).load(function(){
         $('.class-detail-toggle').first().show();
+        $('.instructor .bio').readmore({
+          maxHeight: 110,
+          lessLink: '<a href="#">- Read less</a>',
+          moreLink: '<a href="#">+ Read more</a>',
+        });
       });
+
       // This should open the modal
       $('.button-request-info').click(function(){
         var $nid = $(this).attr('data-nid');
