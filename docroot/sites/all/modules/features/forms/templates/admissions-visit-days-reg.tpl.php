1016604e1f9407b533ea435e61d0905e5d55aac3
Caroline Hecht <cgh2@cornell.edu>
2015-07-29 22:46:55 -0400

    Updates Visit Days dates and email confirmation

---
+++ docroot/sites/all/modules/features/forms/templates/admissions-visit-days-reg.tpl.php
@@ -0,0 +1,46 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for ILR's Admissions Visit Day on
+<?php print $date_attending;?>.  This email serves as your confirmation.
+We have reserved <?php print $num_people;?> spaces for you at this event.
+
+NEED TO GET UPDATED INFO FROM ADMISSIONS
+
+Schedule:
+9:45 a.m. - 10:15 a.m.   Check-in, Ives Hall
+10:15 a.m. - 11:45 a.m.   ILR Admissions Session
+
+Optional tours also offered:
+Tour of the ILR School following the admissions session, approximately 20 minutes.
+Tour of the Cornell University campus.  Tour schedule can be found at  http://www.cornell.edu/visit/#tours
+
+Tips to help you prepare for your visit:
+
+1. Check-in begins at 9:45 a.m. in the 2nd floor lobby of Ives Hall (the ILR School classroom building located on Central Campus, Tower Road).  Please enter Ives Hall through the main entrance on Tower Road between East Avenue and Garden Avenue.  That entrance will lead directly to the 2nd floor of Ives Hall.
+
+2. Allow 15 minutes before check-in for parking and walking to Ives Hall. You can find campus maps and parking information at http://www.cornell.edu/maps/.
+
+3. Arrange lodging in Ithaca as soon as possible if you plan to stay overnight; information is available at http://www.visitithaca.com/lodging/.
+
+4. Wear comfortable shoes if you plan to take a campus tour.
+
+5. On Saturdays, dining facilities on central campus are closed. Please plan to get coffee and breakfast prior to arriving on campus.
+
+6. Light refreshments will be provided after the session.
+
+
+If you have questions, please contact ILR Admissions at 6-7-255-2222 or ilr_admissions@cornell.edu.
+
+We look forward to seeing you on Saturday, <?php print $date_attending;?>.
+
+Cathleen M. Sheils
+Director of Admissions
+ILR School, Cornell University
+216 Ives Hall
+Ithaca, NY 14853
+Phone: (607) 255-2222
+Fax: (607) 254-1251
+http://www.ilr.cornell.edu/admissions
+
+<? print $form_data;?><br><br>
+
