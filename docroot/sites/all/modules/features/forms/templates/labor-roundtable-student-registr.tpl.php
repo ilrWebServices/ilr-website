c2dffa8dc497d41106b85a2bba4b7e85e127fd7c
Caroline Hecht <cgh2@cornell.edu>
2015-09-24 13:26:44 -0400

    Updates Labor Roundtable registration

---
+++ docroot/sites/all/modules/features/forms/templates/labor-roundtable-student-registr.tpl.php
@@ -0,0 +1,13 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the ILR Labor Roundtable.<br>
+
+<? print $event_info_for_email; ?><br><br>
+
+Your information:<br><br>
+<? print $form_data;?><br><br>
+
+If you have any questions, please contact the Roundtable coordinators, ilrroundtablecoord@cornell.edu.
+<br><br>
+Sincerely<br>
+The ILR Labor Roundtable Coordinators
