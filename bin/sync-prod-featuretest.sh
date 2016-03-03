2ffb66f7d2eb77dd6f246174af153e6ef366930f
Aaron Froehlich <aaronfroehlich@gmail.com>
2016-03-03 13:53:50 -0500

    Adds script for syncing between acquia and featuretest

---
+++ bin/sync-prod-featuretest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script runs in the aegir user's home directory on featuretest and
+# enables simple syncing of the prod db
+drush sql-sync @ilr.prod @prod.ilr.featuretest.org --sanitize
+
+drush cc all
+
+drush en featuretest -y
+
+echo "Sync complete."
