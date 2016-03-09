10aa68f291aa23f3dd56d5d29cc6e7120003fe67
Caroline Hecht <cgh2@cornell.edu>
2016-03-09 11:41:06 -0500

    Adds Conversations with Alumni in Finance form to ilr_forms_students

---
+++ docroot/sites/all/modules/features/forms/templates/conversations-alumni-finance.tpl.php
@@ -0,0 +1,12 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the ILR Alumni event "Conversations with Alumni in Finance" with Peter Rogers '86 and Eric Hage '86.<br><br>
+
+Date: Monday, April 18, 2016<br>
+Time: 4:30 PM<br>
+Location: Doherty Lounge, 281 Ives Faculty Building<br><br>
+
+If you have any questions, please contact Sue Sappington, <a href="mailto:ss446@cornell.edu">ss446@cornell.edu</a>.<br><br>
+
+Your registration information is shown below.
+<? print $form_data;?>
