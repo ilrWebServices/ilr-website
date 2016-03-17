1e764d23fbed154891f398416f8cdedee38bced0
Caroline Hecht <cgh2@cornell.edu>
2016-03-17 13:53:42 -0400

    Adds new MPS-NYC Open House rsvp form

---
+++ docroot/sites/all/modules/features/forms/templates/ilr-mps-nyc-open-house-rsvp.tpl.php
@@ -0,0 +1,15 @@
+<?php print $salutation;?><br><br>
+Thank you for your interest in attending our open house and learning more about Cornell ILR's part-time MPS program in NYC. Your RSVP is confirmed. You will receive a reminder and more details closer to the event.<br><br>
+<? print $event_info_for_email; ?><br><br>
+The <a href="http://www.ilr.cornell.edu/academics/degree-programs/graduate-degree-programs/mps-program-new-york-city">MPS program</a> offers a unique opportunity for working professionals to earn a master's degree from the leading school of industrial and labor relations in the country, taught by full-time faculty from the main Cornell campus. A Saturday class schedule in NYC and two-week summer sessions in Ithaca allow practitioners to pursue a graduate degree, without interrupting their careers.<br><br>
+The MPS degree is geared toward experienced professionals from a broad range of occupational backgrounds who share an interest in the dynamics of the workplace. Successful MPS students possess professional proficiency as well as a lively curiosity that motivates them to dig more deeply into the issues that influence the work environment. Through the interdisciplinary curriculum and an individual research project, students gain a framework through which they can analyze the world of work, examine new concepts and ideas for pushing their colleagues and organizations to higher levels, and discover new directions in which their careers can evolve.<br><br>
+Please don’t hesitate to contact me with any questions. We look forward to meeting you!<br><br>
+Best,<br>
+Sara<br><br>
+Sara Edwards<br>
+Program Director<br>
+Cornell MPS/ILR Program in NYC<br>
+212-340-2808<br>
+sara.edwards@cornell.edu<br>
+www.ilr.cornell.edu/mpsnyc<br>
+www.facebook.com/mpsnyc<br>
