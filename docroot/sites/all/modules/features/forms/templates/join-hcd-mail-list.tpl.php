068e77dcc65c266ae09bd302c42bf4a8b2209a0c
Caroline Hecht <cgh2@cornell.edu>
2016-03-17 10:27:54 -0400

    Updates conf page and email wording on HCD new mailing list form

--- docroot/sites/all/modules/features/forms/templates/join-hcd-mail-list.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/join-hcd-mail-list.tpl.php
@@ -1,7 +1,7 @@
 <?php print $salutation;?><br><br>
-Thank you for your request to be added to Cornell ILR's Human Capital Development mailing list. Our training programs help you to gain skills that make an impact in the workplace.<br><br>
+Thank you for your request to be added to Cornell ILR's Human Capital Development mailing list. Our training programs help you gain skills that make an impact in the workplace.<br><br>
 We appreciate your interest in Cornell and hope to welcome you to one of our programs in the near future.<br><br>
-If you have questions, please send email to ILR Customer Service, ilrcustomerservice@cornell.edu, or call us at 866-470-1922.<br><br>
+If you have questions, please email ILR Customer Service, ilrcustomerservice@cornell.edu, or call us at 866-470-1922.<br><br>
 Cornell University<br>
 ILR School<br>
 Human Capital Development<br>
