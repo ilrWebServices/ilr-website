410bfea852c67a18052cae77115dac58327ff83b
Caroline Hecht <cgh2@cornell.edu>
2016-04-22 15:38:41 -0400

    Adds entityform Alumni Student Event Registration

---
+++ docroot/sites/all/modules/features/forms/templates/alumni-events-for-students-reg.tpl.php
@@ -0,0 +1,10 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the following ILR Alumni event:<br><br>
+
+<? print $event_info_for_email; ?><br>
+
+If you have any questions, please contact Sue Sappington, <a href="mailto:ss446@cornell.edu">ss446@cornell.edu</a>.<br><br>
+
+Your registration information is shown below.
+<? print $form_data;?>
