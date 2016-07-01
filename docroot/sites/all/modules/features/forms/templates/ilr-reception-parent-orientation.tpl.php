c4cec7f85e25fdfeb2a20b92b395698f5f8c5d99
Caroline Hecht <cgh2@cornell.edu>
2016-07-01 11:58:56 -0400

    Adds year and email conf file for New Parent Event registration form

---
+++ docroot/sites/all/modules/features/forms/templates/ilr-reception-parent-orientation.tpl.php
@@ -0,0 +1,14 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the ILR Reception for New Parents.<br>
+
+<? print $event_info_for_email; ?><br><br>
+
+Your information:<br><br>
+<? print $form_data;?><br><br>
+
+If you have any questions, please contact Sue Sappington, <a href="mailto:ss446@cornell.edu">ss446@cornell.edu</a>.
+<br><br>
+Sincerely<br>
+Sue Sappington
+ILR Alumni Affairs Development Office
