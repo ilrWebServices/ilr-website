d44c3a44df37718da05b6ca19837df2e49d4400a
fjp2 <fjp2@cornell.edu>
2016-11-04 16:09:15 -0400

    Highlights first word in list of value propositions on a certificate program

--- docroot/sites/all/modules/features/ilr_certificate_programs/ilr_certificate_programs.js
+++ docroot/sites/all/modules/features/ilr_certificate_programs/ilr_certificate_programs.js
@@ -17,6 +17,12 @@
         position_call_to_action();
       });
 
+      $(window).load(function() {
+        if ($('div.pitch ul li').length) {
+          highlight_first_word();
+        }
+      });
+
       function position_call_to_action() {
         is_in_sidebar = function (el) {
           return el.parent().has('div#main');
@@ -30,6 +36,33 @@
         } else if ( is_in_sidebar(sidebar_second) ) {
           $('#sidebar-first').after(sidebar_second);
         }
+      }
+
+      function highlight_first_word() {
+        // prepare the first word in a list of items to be bolded
+        $('div.pitch ul li').each(function(index, element) {
+          var word_array, first_word, middle_part, first_word_class;
+          var phrase = $(element).html().trim();
+
+          // split the phrase on spaces
+          word_array = phrase.split(/\s+/);
+
+          // isolate the first word
+          first_word = word_array.shift();
+          first_word_class = 'first-word';
+
+          first_word = '<span class="' + first_word_class + '">' + first_word + '</span>' + ' ';
+
+          // check for additional words
+          if (word_array.length > 0) {
+            middle_part = word_array;
+          }
+
+          // rejoin the parts of the phrase. convert commas into spaces
+          phrase = [first_word, middle_part].join('').replace(/,/g, ' ');
+
+          $(element).html(phrase);
+        });
       }
     }
   };
