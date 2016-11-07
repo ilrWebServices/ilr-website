083df0b9af346c973700a10bf75220ffb4ac6f05
Caroline Hecht <cgh2@cornell.edu>
2016-11-07 14:46:12 -0500

    Adds MILR Info Session Registration form and email conf file

---
+++ docroot/sites/all/modules/features/forms/templates/grad-info-session-registration.tpl.php
@@ -0,0 +1,14 @@
+<?php print $salutation;?><br><br>
+
+Thank you for registering for the upcoming information session for the Cornell University
+ILR School Master of Industrial and Labor Relations (MILR) program.
+<br><br>
+<? print $event_info_for_email; ?>
+<br><br>
+The webinar connection information will be sent to you in advance of the information session. Please contact us if you have any questions.<br><br>
+
+Sincerely,<br>
+Graduate Programs Office<br>
+ILR School at Cornell University<br>
+ilrgradapplicant@cornell.edu<br>
+607-255-1522
