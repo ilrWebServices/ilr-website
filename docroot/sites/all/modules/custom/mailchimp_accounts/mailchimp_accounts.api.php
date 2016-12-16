917cff3aa6cce06ead5b7c538be842a0e87c3992
Aaron Froehlich <aaronfroehlich@gmail.com>
2016-12-16 14:33:35 -0500

    Updates mailchimp_accounts api key hook implementation

---
+++ docroot/sites/all/modules/custom/mailchimp_accounts/mailchimp_accounts.api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Mailchimp Accounts hook definitions.
+ */
+
+/**
+ * Register API key(s) for MailChimp Accounts
+ *
+ * @return array
+ *   The keys are the account id and the values are the API keys
+ */
+function hook_mailchimp_accounts_api_key() {
+
+}
