9d44752e9889972c4732e4cba550014482e59155
Caroline Hecht <cgh2@cornell.edu>
2017-03-10 14:33:39 -0500

    Updates forensics payment form and alumni student reg form in small ways

--- docroot/sites/all/modules/features/forms/templates/forensics-debate-camp-payment.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/forensics-debate-camp-payment.tpl.php
@@ -2,10 +2,12 @@
 
 Thank you for your payment for the Cornell International Summer Debate Camp.<br><br>
 
-One final step is to list your travel information so we can help arrange transportation. Remember, we will only provide transportation to and from Ithaca Tompkins Regional Airport, Corning/Elmira Regional Airport and Syracuse Hancock International Airport. Those flying in or out of other airports must arrange their own transit to and from Ithaca. Please complete this Travel Information Form to let us know your travel plans: <br>
-http://goo.gl/forms/yAn2V6WbQE<br><br>
+One final step is to provide your travel details to ensure that Cornell camp staff are able to arrange for pick-up and transportation. Please note that Cornell camp staff will only provide transportation to and from Ithaca Tompkins Regional Airport, Corning/Elmira Regional Airport and Syracuse Hancock International Airport. Those flying in or out of other airports must arrange their own transit to and from Ithaca.<br><br>
 
-Please review the information below, and contact Lindsay Bing, lb542@cornell.edu, if you have any questions.<br><br>
+Please complete this Travel Information Form to let us know your travel plans: <br>
+https://goo.gl/forms/6yQBfUpN0SccrgIG2<br><br>
+
+Please review the information below, and contact Ming Shiao, debate@cornell.edu, if you have any questions.<br><br>
 
 Summer Camp Details:<br><br>
 <? print $event_info_for_email; ?><br>
@@ -17,12 +19,11 @@ Registration Information:<br><br>
 <? print $form_data;?><br><br>
 
 Thank you, and don't forget to fill out the Travel Information Form!<br>
-http://goo.gl/forms/yAn2V6WbQE<br><br>
+https://goo.gl/forms/6yQBfUpN0SccrgIG2<br><br>
 
-Lindsay Bing<br>
-Assistant Director of Forensics Speech &amp; Debate Programs<br>
-School of Industrial Labor Relations<br>
+Ming Shiao<br>
+Outreach Director, Cornell Speech and Debate<br>
 Cornell University<br>
 Ives Hall 615b<br>
-lb542@cornell.edu<br>
-+01(607)255-7512
+debate@cornell.edu<br>
++01(607)255-2136
