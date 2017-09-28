b29e3c9b003bf6da01c2fb568fe6c0b30ee77a1b
Aaron Froehlich <aaronfroehlich@gmail.com>
2017-09-28 10:36:25 -0400

    Fixes forensics debate payment form and updates the email template

--- docroot/sites/all/modules/features/forms/templates/forensics-debate-camp-payment.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/forensics-debate-camp-payment.tpl.php
@@ -1,15 +1,10 @@
 <?php print $salutation;?><br><br>
 
-Thank you for your payment for the Cornell International Summer Debate Camp.<br><br>
-
-One final step is to provide your travel details to ensure that Cornell camp staff are able to arrange for pick-up and transportation. Please note that Cornell camp staff will only provide transportation to and from Ithaca Tompkins Regional Airport, Corning/Elmira Regional Airport and Syracuse Hancock International Airport. Those flying in or out of other airports must arrange their own transit to and from Ithaca.<br><br>
-
-Please complete this Travel Information Form to let us know your travel plans: <br>
-https://goo.gl/forms/6yQBfUpN0SccrgIG2<br><br>
+Thank you for your payment for the Cornell Debate Team.<br><br>
 
 Please review the information below, and contact Ming Shiao, debate@cornell.edu, if you have any questions.<br><br>
 
-Summer Camp Details:<br><br>
+Details:<br><br>
 <? print $event_info_for_email; ?><br>
 
 Payment Information:<br><br>
@@ -18,8 +13,7 @@ Payment Information:<br><br>
 Registration Information:<br><br>
 <? print $form_data;?><br><br>
 
-Thank you, and don't forget to fill out the Travel Information Form!<br>
-https://goo.gl/forms/6yQBfUpN0SccrgIG2<br><br>
+Thank you.<br><br>
 
 Ming Shiao<br>
 Outreach Director, Cornell Speech and Debate<br>
