151e7f6e34caddfd1bb77949b39879a0ba278fb6
David DeMello <daviddemello@gmail.com>
2017-10-02 13:47:06 -0400

    Adds ILR theme grant application form

---
+++ docroot/sites/all/modules/features/forms/templates/theme-grant-application.tpl.php
@@ -0,0 +1,7 @@
+<?php print $salutation;?><br><br>
+
+Thank you for your proposal and grant application for the ILR Theme Project.<br><br>
+
+Below is the content of your submission.<br><br>
+
+<? print $form_data;?><br><br>
