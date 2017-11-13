693ea460f4e89cf298beb096e96b61bebfee3875
David DeMello <daviddemello@gmail.com>
2017-11-13 05:19:49 -0500

    Introduces ILR theme project book suggestion entity form

---
+++ docroot/sites/all/modules/features/forms/templates/ilr_theme_project_book_suggestio.tpl.php
@@ -0,0 +1,7 @@
+<?php print $salutation;?><br><br>
+
+Thank you for your book suggestion for the ILR Theme Project.<br><br>
+
+Below is the content of your submission.<br><br>
+
+<? print $form_data;?><br><br>
