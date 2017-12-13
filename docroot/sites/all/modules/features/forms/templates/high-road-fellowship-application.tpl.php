778fa76c44ae7f04bb7b6a142f0f1d3e2cc58d13
David DeMello <daviddemello@gmail.com>
2017-12-13 12:25:46 -0500

    Introduces High Road Fellowship application form.

---
+++ docroot/sites/all/modules/features/forms/templates/high-road-fellowship-application.tpl.php
@@ -0,0 +1,7 @@
+<?php print $salutation;?><br><br>
+
+This is a confirmation from the ILR School at Cornell University of your application for an Engaged Learning and Research Fellowship for Summer 2018 in Buffalo, NY.<br><br>
+
+<? print $form_data;?><br><br>
+
+If you have any questions, please contact Megan Connelly at mlc322@cornell.edu.
