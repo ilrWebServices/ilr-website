7abfe3fd5ddb0bee433c3f4f6fdd0365ae96f6d7
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-01-05 14:39:18 -0500

    Refactors mobile version for slc site

--- docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
+++ docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
@@ -4,31 +4,31 @@
  * The template file for the sports leadership header.
  */
 ?>
-<!-- <h1>Content goes here</h1> -->
 <div class="slc-banner">
-	<div class="slc-banner__heading">
-		<h2>ILR</h2>
+  <div class="slc-banner__content">
+    <div class="slc-banner__heading">
+      <h2 class="slc-banner__brand-primary">ILR</h2>
+      <h1 class="slc-banner__brand-secondary"><span>Sports Leadership</span> Camp</h1>
+    </div>
+    <p class="slc-banner__dates">July 29 - August 4, 2018</p>
+    <div class="slc-banner__logos">
+      <ul>
+        <li><img src="/sites/all/themes/ilr_theme/images/ILR-logo.png" /></li>
+        <li><img src="/sites/all/themes/ilr_theme/images/CU-athletics-logo.png" /></li>
+      </ul>
+    </div>
+    <div class="slc-banner__link">
+      <a class="button" href="">Apply Now!</a>
+    </div>
+  </div>
+  <div class="slc-banner__media">
+    <div class="slc-banner__overlay"></div>
+    <ul class="slc-banner__images">
+      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-football.jpg"></li>
+      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-students.jpg"></li>
+      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-leaders.jpg"></li>
+      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-teaching.jpg"></li>
+    </ul>
+  </div>
 
-		<h1><em>Sports Leadership</em> Camp</h1>
-
-		<p>July 29 - August 4, 2018</p>
-	</div>
-
-	<div class="slc-banner__item">
-		<div class="slc-banner__logos">
-			<ul>
-				<li><img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/ILR-logo.png" /></li>
-				<li><img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/CU-athletics-logo.png" /></li>
-			</ul>
-		</div>
-
-		<div class="slc-banner__link">
-		<a class="button" href="">Apply Now!</a>
-		</div>
-	</div>
-
-	<div class="slc-banner__image">
-		<!-- single animated gif, hide on desktop and tablet -->
-		<img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/animated-gif.gif" />
-	</div>
 </div>
