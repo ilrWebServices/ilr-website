3017d2eb92cb18ff23a09b40a66efcf2be48e697
john <fjp2@cornell.edu>
2018-01-05 10:22:42 -0500

    Adds custom banner to sports camp subsite

--- docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
+++ docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
@@ -4,4 +4,31 @@
  * The template file for the sports leadership header.
  */
 ?>
-<h1>Content goes here</h1>
+<!-- <h1>Content goes here</h1> -->
+<div class="slc-banner">
+	<div class="slc-banner__heading">
+		<h2>ILR</h2>
+
+		<h3><em>Sports Leadership</em> Camp</h3>
+
+		<p>July 29 - August 4, 2018</p>
+	</div>
+
+	<div class="slc-banner__item">
+		<div class="slc-banner__logos">
+			<ul>
+				<li><img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/CU-athletics-logo.png" /></li>
+				<li><img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/CU-athletics-logo.png" /></li>
+			</ul>
+		</div>
+
+		<div class="slc-banner__link">
+		<a class="button" href="">Apply Now!</a>
+		</div>
+	</div>
+
+	<div class="slc-banner__image">
+		<!-- single animated gif, hide on desktop and tablet -->
+		<img src="http://www.ilr-website.dev/sites/all/themes/ilr_theme/images/animated-gif.gif" />
+	</div>
+</div>
