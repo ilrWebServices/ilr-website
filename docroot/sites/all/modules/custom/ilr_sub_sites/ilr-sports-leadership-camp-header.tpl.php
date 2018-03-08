94caa244ea942d787298b3e0e1205369b375d325
David DeMello <daviddemello@gmail.com>
2018-03-08 14:10:59 -0500

    Changes banner of sports camp to indicate 2018 closure and name change to academy.

--- docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
+++ docroot/sites/all/modules/custom/ilr_sub_sites/ilr-sports-leadership-camp-header.tpl.php
@@ -8,26 +8,17 @@
   <div class="slc-banner__content">
     <div class="slc-banner__heading">
       <h2 class="slc-banner__brand">Cornell ILR</h2>
-      <h1 class="slc-banner__brand"><a href="/sports-leadership-camp"><span>Sports Leadership</span> Camp</a></h1>
+      <h1 class="slc-banner__brand"><a href="/sports-leadership-camp"><span>Sports Leadership</span> Academy</a></h1>
     </div>
-    <p class="slc-banner__dates">July 29 - August 4, 2018</p>
-    <div class="slc-banner__logos">
-      <ul>
-        <li><img src="/sites/all/themes/ilr_theme/images/ILR-logo.png" /></li>
-        <li><img src="/sites/all/themes/ilr_theme/images/CU-athletics-logo.png" /></li>
-      </ul>
-    </div>
+    <p class="slc-banner__dates">Registration Closed for 2018</p>
     <div class="slc-banner__link">
-      <a class="button" href="/sports-leadership-camp/apply">Apply Now!</a>
+      <a class="action-callout__button" href="#">Ask about Summer 2019</a>
     </div>
   </div>
   <div class="slc-banner__media">
     <div class="slc-banner__overlay"></div>
     <ul class="slc-banner__images">
-      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-football.jpg"></li>
-      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-students.jpg"></li>
-      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-leaders.jpg"></li>
-      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-teaching.jpg"></li>
+      <li class="slc-banner__image"><img src="/sites/all/themes/ilr_theme/images/slc-banner.jpg"></li>
     </ul>
   </div>
 
