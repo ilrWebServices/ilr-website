e47a62c02ea93ef43509dd8f6acd8f7a7fc3f377
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-03-12 16:01:04 -0400

    Sanitizes redhen contacts and removes freedompay data

--- bin/sync-prod-featuretest.sh
+++ bin/sync-prod-featuretest.sh
@@ -2,7 +2,7 @@
 
 # This script runs in the aegir user's home directory on featuretest and
 # enables simple syncing of the prod db
-drush sql-sync @ilr.prod @prod.ilr.featuretest.org --sanitize --sanitize-password=SEE_FILE_ON_SERVER --structure-tables-list=cache,cache_*,sessions,entityform -y
+drush sql-sync @ilr.prod @prod.ilr.featuretest.org --sanitize --sanitize-password=SEE_FILE_ON_SERVER --structure-tables-list=cache,cache_*,sessions,entityform,freedompay_*,redhen_* -y
 
 drush cc all
 
@@ -10,5 +10,8 @@ drush @ilr.prod.featuretest.org en featuretest -y
 
 # Sanitize form data
 drush @ilr.prod.featuretest.org sfd -y
+
+# Sanitize contact data
+drush @ilr.prod.featuretest.org scd -y
 
 echo "Sync complete."
