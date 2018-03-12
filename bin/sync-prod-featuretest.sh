9f59b24def66e50717b2fbeca1357c157818aacb
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-03-12 12:57:29 -0400

    Updates db sync to featuretest to sanitize form data

--- bin/sync-prod-featuretest.sh
+++ bin/sync-prod-featuretest.sh
@@ -2,10 +2,13 @@
 
 # This script runs in the aegir user's home directory on featuretest and
 # enables simple syncing of the prod db
-drush sql-sync @ilr.prod @prod.ilr.featuretest.org --sanitize --sanitize-password=SEE_FILE_ON_SERVER --structure-tables-list=cache,cache_*,sessions -y
+drush sql-sync @ilr.prod @prod.ilr.featuretest.org --sanitize --sanitize-password=SEE_FILE_ON_SERVER --structure-tables-list=cache,cache_*,sessions,entityform -y
 
 drush cc all
 
-drush en featuretest -y
+drush @ilr.prod.featuretest.org en featuretest -y
+
+# Sanitize form data
+drush @ilr.prod.featuretest.org sfd -y
 
 echo "Sync complete."
