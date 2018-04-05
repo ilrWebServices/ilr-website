4e12950cdabc6ae74ae32851484c292086eb72ac
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-04-05 10:22:07 -0400

    Turns social callout hashtags into links

--- docroot/sites/all/themes/ilr_theme/templates/paragraphs-item--social-callout.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/paragraphs-item--social-callout.tpl.php
@@ -14,5 +14,5 @@
     <li class="social-callout__item"><a class="follow-<?php echo strtolower($name); ?> social-callout__link" href="<?php echo $url; ?>" title="<?php echo $name; ?>" style="color:#<?php echo $bg_color; ?>;"><span><?php echo $name; ?></span></a></li>
     <?php endforeach; ?>
   </ul>
-  <p class="social-callout__hashtag">#<?php echo $hashtag; ?></p>
+  <p class="social-callout__hashtag"><a class="social-callout__hashtag-link" href="//twitter.com/hashtag/<?php echo $hashtag; ?>">#<?php echo $hashtag; ?></a></p>
 </div>
