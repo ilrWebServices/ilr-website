96173ba69a11e906eb3e370b009ab7685f8ed05a
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-07-27 16:14:23 -0400

    Adds read more link for homepage news

--- docroot/sites/all/themes/open_frame/templates/block--bean--homepage-honeycomb-test.tpl.php
+++ docroot/sites/all/themes/open_frame/templates/block--bean--homepage-honeycomb-test.tpl.php
@@ -1,6 +1,7 @@
-<div class="homepage-news tagged-content block-bean">
+<div class="homepage-news tagged-content">
   <h2 class="news news__heading news__heading--homepage">ILR<br /> News</h2>
   <?php
     print render($content);
   ?>
+  <div class="homepage-news__link"><a class="button button--framed button--centered" href="/news">More ILR News</a></div>
 </div>
