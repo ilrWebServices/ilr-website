5b27594973a6c2f02119cebcad693301b9fcf8ca
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-08-15 16:13:38 -0400

    Updates links in headers and fixes image links

--- docroot/sites/all/themes/open_frame/templates/page.tpl.php
+++ docroot/sites/all/themes/open_frame/templates/page.tpl.php
@@ -7,7 +7,7 @@
   </div>
   <div class="container">
     <div class="logo-wrapper">
-      <?php ?>
+      <a class='cornell' href="https://cornell.edu"></a>
       <a class="ilr" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
          <img src="<?php echo $logo?>" alt="Cornell University | ILR School" title="Cornell University | ILR School">
       </a>
