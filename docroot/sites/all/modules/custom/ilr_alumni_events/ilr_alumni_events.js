e4c223924c49e71a01f16f4cd92bb6564dc69ca2
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-09-13 18:38:15 -0400

    Adds sports summit payment form

--- docroot/sites/all/modules/custom/ilr_alumni_events/ilr_alumni_events.js
+++ docroot/sites/all/modules/custom/ilr_alumni_events/ilr_alumni_events.js
@@ -3,28 +3,78 @@
     attach: function (context, settings) {
       if (Drupal.settings.ilr_alumni_events) {
         var memberPrice = Drupal.settings.ilr_alumni_events.memberPrice;
+        var nonMemberPrice = Drupal.settings.ilr_alumni_events.nonMemberPrice;
+        var memberCount = $('select[name="field_tickets_for_aa_members[und]"]').val();
+        var nonMemberCount = $('select[name="field_tickets_for_non_members[und]"]').val();
+        var memberTotal, nonMemberTotal;
 
         function updatePrices() {
           $('.alumni-event-pricing').html('');
-          updateMemberPrice();
+          updateMemberPriceDisplay();
+          updateNonMemberPriceDisplay();
           updateTotalPrice();
         }
 
         function getMemberCost() {
-          return $('select[name="field_tickets_for_aa_members[und]"]').val() * memberPrice;
+          if (memberCount == '_none') {
+            memberCount = 0;
+          }
+          memberTotal = memberCount * memberPrice;
+          return memberTotal;
+        }
+
+        function getNonMemberCost() {
+          nonMemberTotal = 0;
+          if (nonMemberCount > 0) {
+            var diffCount = nonMemberCount - memberCount; 2 - 1
+            var discounted = (diffCount <= 0)
+              ? nonMemberCount * memberPrice
+              : memberTotal;
+            var remaining = (diffCount > 0)
+              ? diffCount * nonMemberPrice
+              : 0;
+            nonMemberTotal = discounted + remaining;
+          }
+          return nonMemberTotal;
         }
 
         function getTotalCost() {
-          return getMemberCost();
+          return getMemberCost() + getNonMemberCost();
         }
 
-        function updateMemberPrice() {
+        function updateMemberPriceDisplay() {
           if (memberPrice == 0) {
             content = 'Free';
           } else {
             content = 'x $'+memberPrice+' = $'+getMemberCost();
           }
           $('select[name="field_tickets_for_aa_members[und]"]').after(' <span class="alumni-event-pricing member-price">'+content+'</span>');
+        }
+
+        function updateNonMemberPriceDisplay() {
+          if (nonMemberCount > 0) {
+            if (nonMemberPrice == 0) {
+              content = 'Free';
+            } else {
+              if (memberCount) {
+                var diffCount = nonMemberCount - memberCount;
+                if (diffCount <= 0) { // All non-members get discount
+                  content = nonMemberCount + ' tickets x $' + memberPrice + ' = $'+ nonMemberCount * memberPrice + ' <br />';
+                }
+                else { // Some get discount, some pay higher rate
+                  content = memberCount + ' tickets x $' + memberPrice + ' = $'+ getMemberCost() + ' <br />';
+                  content += diffCount + ' ticket(s) x $' + nonMemberPrice+' = $'+ diffCount * nonMemberPrice;
+                }
+              }
+              else { // None get discount
+                content = nonMemberCount + ' ticket(s) x $' + nonMemberPrice+' = $'+getNonMemberCost();
+              }
+            }
+          }
+          else {
+            content = '';
+          }
+          $('select[name="field_tickets_for_non_members[und]"]').after(' <span class="alumni-event-pricing non-member-price">'+content+'</span>');
         }
 
         function updateTotalPrice() {
@@ -36,6 +86,12 @@
         updatePrices();
 
         $('select[name="field_tickets_for_aa_members[und]"]').change(function(){
+          memberCount = $('select[name="field_tickets_for_aa_members[und]"]').val();
+          updatePrices();
+        });
+
+        $('select[name="field_tickets_for_non_members[und]"]').change(function(){
+          nonMemberCount = $('select[name="field_tickets_for_non_members[und]"]').val();
           updatePrices();
         });
       }
