73e6a88269254a76e4ebedb3aa185566c684d127
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-11-13 10:23:53 -0500

    Reworks homepage news implementation for manual content listing

---
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
@@ -0,0 +1,4 @@
+<div class="homepage-news tagged-content">
+  <?php print render($content); ?>
+  <div class="homepage-news__link"><a class="button button--framed" href="/news">More ILR News</a></div>
+</div>
