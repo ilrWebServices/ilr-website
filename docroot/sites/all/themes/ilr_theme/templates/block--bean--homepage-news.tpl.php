a99be8237f893054ad939a32a862dfc05dc37d7d
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-11-15 10:44:15 -0500

    Removes legacy square thumbnail implementation

--- docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
@@ -1,4 +1,4 @@
-<div class="homepage-news tagged-content">
+<div class="homepage-news">
   <?php print render($content); ?>
   <div class="homepage-news__link"><a class="button button--framed" href="/news">More ILR News</a></div>
 </div>
