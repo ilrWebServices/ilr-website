aa4d369d443c46742597632103d1ab5115f349e5
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-11-19 11:30:16 -0500

    Adds image to course concentrations and styles as cards

---
+++ docroot/sites/all/themes/ilr_theme/templates/taxonomy-term--course-concentration.tpl.php
@@ -0,0 +1,7 @@
+<div id="taxonomy-term-<?php print $term->tid; ?>" class="<?php print $classes; ?>">
+
+  <?php if (!$page): ?>
+    <h2><a href="<?php print $term_url; ?>"><?php print $term_name; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($content); ?>
+</div>
