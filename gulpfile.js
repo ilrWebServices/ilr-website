a15dab734b0e375f2741d2abbf65eb3d9205f536
Jeff Amaral <jeff.amaral@cornell.edu>
2018-12-13 08:14:21 -0800

    Clean up gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,5 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
-var livereload = require('gulp-livereload');
-
-// var localConfig = {
-//     cssConfig: {
-//       includePaths: [
-//         'node_modules/breakpoint-sass/stylesheets/',
-//         'node_modules/singularitygs/stylesheets/',
-//         'node_modules/modularscale-sass/stylesheets',
-//         'node_modules/sass-tools/lib',
-//         // 'node_modules/singularity-extras/stylesheets/',
-//         'node_modules/compass-mixins/lib/',
-//         'node_modules/float-label-css/scss/',
-//         './node_modules'
-//       ]
-//     },
-//     browserSync: {
-//       // We want the local static server for Pattern Lab to serve from the same place as Drupal does: the `./web` folder in the base of the repo (which is `../../../` from here). This allows us to load JS libraries like drupal.js & jQuery.js from the `/core` directory).
-//       startPath: 'themes/custom/prism/pattern-lab/public/',
-//       baseDir: '../../../',
-//       openBrowserAtStart: true
-//     }
-//   };
 
 gulp.task('sass', function() {
   return gulp.src('docroot/sites/all/themes/ilr_theme/scss/style.scss')
@@ -36,12 +14,12 @@ gulp.task('sass', function() {
     })
     .on('error', plugins.sass.logError))
     .pipe(gulp.dest('docroot/sites/all/themes/ilr_theme/css'))
-    .pipe(livereload());
+    .pipe(plugins.livereload());
   });
 
 gulp.task('default', ['sass'], function() {
   console.log('Watching for .scss file changes in /scss.');
-  livereload.listen({
+  plugins.livereload.listen({
     'port': 35777
   });
   gulp.watch(['docroot/sites/all/themes/ilr_theme/scss/**/*.scss'], ['sass']);
