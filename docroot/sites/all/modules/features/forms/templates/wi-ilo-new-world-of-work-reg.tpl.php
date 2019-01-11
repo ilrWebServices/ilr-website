c9b24a487d2af3e6bc2df1c5e6a6c54ef6a5cdd1
David DeMello <daviddemello@gmail.com>
2019-01-11 12:39:08 -0500

    Updates ilr_forms_public feature and adds photo release statement to the confirmation message for the 01/31/2018 ILO event registration.

--- docroot/sites/all/modules/features/forms/templates/wi-ilo-new-world-of-work-reg.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/wi-ilo-new-world-of-work-reg.tpl.php
@@ -6,7 +6,9 @@ While open registration for this event has closed, we have added you to the wait
 
 You will be notified via email if a space opens up.<br><br>
 
-Thank you.<br><br>
+Thank you.<br><br><br><br>
+
+N.B. This is a public event, which will be photographed, livestreamed, filmed and/or otherwise recorded. Your registration for this event constitutes your consent to such photography, livestreaming, filming and/or recording and to any use, in any and all media.
 
 
 
