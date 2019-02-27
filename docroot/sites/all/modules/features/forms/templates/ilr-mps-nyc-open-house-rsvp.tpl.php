fe68ca59d87908d8f7aee7d1893772660dbdf8fa
David DeMello <daviddemello@gmail.com>
2019-02-27 11:28:39 -0500

    Modifies response email from the MPS-NYC open house rsvp entity form.

--- docroot/sites/all/modules/features/forms/templates/ilr-mps-nyc-open-house-rsvp.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/ilr-mps-nyc-open-house-rsvp.tpl.php
@@ -1,15 +1,17 @@
 <?php print $salutation;?><br><br>
 Thank you for your interest in attending our open house and learning more about Cornell ILR's part-time MPS program in NYC. Your RSVP is confirmed. You will receive a reminder and more details closer to the event.<br><br>
 <? print $event_info_for_email; ?><br><br>
-The <a href="http://www.ilr.cornell.edu/academics/degree-programs/graduate-degree-programs/mps-program-new-york-city">MPS program</a> offers a unique opportunity for working professionals to earn a master's degree from Cornell’s ILR School, taught by full-time faculty from the main upstate campus. A Saturday class schedule in NYC and two-week summer sessions in Ithaca allow practitioners to pursue a graduate degree, without interrupting their careers.<br><br>
-The MPS degree is geared toward experienced professionals from a broad range of occupational backgrounds who share an interest in organizational excellence. By providing a 360-degree view of how organizations work effectively, the program is ideal for working professionals looking to build on their technical competencies for more strategic management roles. Subject experts become organizational leaders. Successful MPS students possess professional proficiency as well as a lively curiosity that motivates them to dig more deeply into the issues that influence the work environment. Through the multidisciplinary curriculum and specialized research project, students gain frameworks through which they can analyze the world of work, examine new concepts and ideas for pushing their colleagues and organizations to higher levels, and discover new directions in which their careers can evolve.<br><br>
+The <a href="https://www.ilr.cornell.edu/programs/graduate-degree-programs/mps-program-new-york-city">MPS program</a> offers a unique opportunity for working professionals to earn a master's degree from Cornell’s ILR School, taught by full-time faculty from the main upstate campus. A Saturday class schedule in NYC and two-week summer sessions in Ithaca allow practitioners to pursue a graduate degree, without interrupting their careers.<br><br>
+The MPS degree is geared toward experienced professionals from a broad range of careers and across sectors who share an interest in organizational excellence. By providing a 360-degree view of how organizations work effectively, the program is ideal for practitioners looking to build on their technical competencies for more strategic management roles. Subject experts become organizational leaders. Successful MPS students possess professional proficiency as well as a lively curiosity that motivates them to dig more deeply into the issues that influence the work environment. Through the multidisciplinary curriculum and specialized capstone project, students gain frameworks through which they can analyze the world of work, examine new concepts and ideas for pushing their colleagues and organizations to higher levels, and discover new directions in which their careers can evolve.<br><br>
 Please don’t hesitate to contact me with any questions. We look forward to meeting you!<br><br>
 Best,<br>
 Sara<br><br>
 Sara Edwards<br>
-Program Director<br>
-Cornell MPS/ILR Program in NYC<br>
-212-340-2808<br>
-sara.edwards@cornell.edu<br>
+MPS/ILR Program in NYC<br>
+Institute for Workplace Studies<br>
+Cornell University ILR<br>
+212-340-2808 | sara.edwards@cornell.edu<br><br>
 www.ilr.cornell.edu/mpsnyc<br>
 www.facebook.com/mpsnyc<br>
+<b style="font-size:10.0pt;font-family:Arial,Helvetica,sans-serif">It takes
+<span style="color:#b31b1b">work</span>.</b>
