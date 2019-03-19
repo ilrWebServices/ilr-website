101f2bb95cb88f0c2322ea9882a17bd859a3c8ac
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-03-19 14:12:31 -0400

    Additional config_dir fixes for platform

--- config/get_dynamic_allowed_values.php
+++ config/get_dynamic_allowed_values.php
@@ -3,7 +3,7 @@
 if (!defined('CONFIG_PATH')) {
   if (isset($_ENV['PLATFORM_PROJECT'])) {
     // We're on Platform.sh
-    define('CONFIG_PATH', '/ilr_config');
+    define('CONFIG_PATH', '/app/ilr_config');
   } // Check for featuretest
   else if (strpos($_SERVER['SERVER_NAME'], 'featuretest.org') > 0) {
     define('CONFIG_PATH', '/var/aegir/projects/ilr/prod/config');
