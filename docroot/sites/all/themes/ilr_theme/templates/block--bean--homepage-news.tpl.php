42b808763eadc848b61d2265e09a7f1e2e5ee941
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-03-27 10:26:11 -0400

    Fix contextual links editing issues
    Add padding to ensure enough space
    Render title suffix on homepage news

--- docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
@@ -1,4 +1,5 @@
-<div class="homepage-news">
+<div class="homepage-news <?php print $classes; ?>">
+  <?php print render($title_suffix); ?>
   <?php print render($content); ?>
   <div class="homepage-news__link"><a class="button button--framed" href="/news">More ILR News</a></div>
 </div>
