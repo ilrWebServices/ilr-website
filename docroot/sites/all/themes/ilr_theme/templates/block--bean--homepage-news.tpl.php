4fca5daee13f890dc4de2e241a75763b539e2f48
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-06-14 12:04:05 -0400

    Conform homepage news block to new design

--- docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
@@ -1,5 +1,10 @@
 <div class="homepage-news <?php print $classes; ?>">
   <?php print render($title_suffix); ?>
+  <div class="cu-section-heading--framed cu-section-heading--framed-right cu-section-heading">
+    <h2 class="cu-section-heading__heading">ILR News</h2>
+    <a href="/news" class="cu-section-heading__link">
+      View All News
+    </a>
+  </div>
   <?php print render($content); ?>
-  <div class="homepage-news__link"><a class="button button--framed" href="/news">More ILR News</a></div>
 </div>
