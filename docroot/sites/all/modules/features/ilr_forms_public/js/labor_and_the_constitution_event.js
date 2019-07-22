9b7ac61c263eb655ed3eb61f602a8242605b2819
David DeMello <daviddemello@gmail.com>
2019-07-22 12:10:09 -0400

    Adds styles, Javascript, and hooks for Labor and the Consitution conference page and registration form.

---
+++ docroot/sites/all/modules/features/ilr_forms_public/js/labor_and_the_constitution_event.js
@@ -0,0 +1,36 @@
+(function ($) {
+  Drupal.behaviors.labor_and_the_constitution_event = {
+    attach: function (context, settings) {
+      $(document).ready(function() {
+        set_price();
+        $('#edit-field-discount-eligible-und').change( function() {
+          set_price();
+        });
+
+        function set_price() {
+          var currentPrice, priceNote;
+          var early_bird_deadline = new Date('8/26/2019');
+          var today = new Date();
+
+          // Set full price based on date.
+          if ( (early_bird_deadline > today) ) {
+            currentPrice = 275;
+            priceNote = 'Registration fee ($275 until 8/26/19, $325 thereafter)'
+          } else {
+            currentPrice = 325;
+            priceNote = 'Registration fee ($325)'
+          }
+
+          // Toggle full and discounted price
+          if ( $('#edit-field-discount-eligible-und').is(':checked') ) {
+            $('#edit-field-total-cost-und-0-value').val('25');
+            $('#field-total-cost-add-more-wrapper').find('label').text('Registration fee ($25 student rate)');
+          } else {
+            $('#edit-field-total-cost-und-0-value').val(currentPrice);
+            $('#field-total-cost-add-more-wrapper').find('label').text(priceNote);
+          }
+        }
+      });
+    }
+  };
+}(jQuery));
