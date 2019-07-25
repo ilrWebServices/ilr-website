bb421934b58f20e51629888aabb0cb66adb370ae
David DeMello <daviddemello@gmail.com>
2019-07-25 13:14:22 -0400

    Changes email confirmation message to submitters of the entity form ilr_visit_day_registration

--- docroot/sites/all/modules/features/forms/templates/ilr-visit-day-registration.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/ilr-visit-day-registration.tpl.php
@@ -1,34 +1,11 @@
 <?php print $salutation;?><br><br>
 
-We look forward to meeting you at the ILR School’s Saturday Visit Days program. During the program, you will learn how you can tailor the ILR curriculum to your interests, pursue internship and career opportunities around the world, and prepare a strong application. Current students and recent graduates will also be available to answer your questions and share their experience as ILR students.
-<br><br>
+Thank you for registering for an ILR Saturday Visit Day program. More information about each event will be provided by email.<br><br>
 
-<strong>Please review this information as you make your plans to attend a session.</strong><br><br>
+Feel free to contact the ILR Admissions Office at ilradmissions@cornell.edu or (607) 255-2222 if you have any questions. <br><br>
 
-<strong>Ithaca, NY- Cornell University</strong><br>
-Check-in takes place between 9:00 – 9:45 a.m. in the lobby of Ives Hall (enter through the main entrance on Tower Rd.). The program will run from 10:00 - 11:30 a.m. Please note morning refreshments will not be served. Coffee/dining options near Ives Hall are limited, so plan to have breakfast and purchase coffee before arriving on campus. Review the times and locations of campus dining facilities <a href="http://living.cornell.edu/dine/">here</a>.<br><br>
-
-Optional <a href="http://www.cornell.edu/visit/">campus tours</a> begin at 9:00 a.m. or 1:00 p.m. and leave from Day Hall (a 5 minute walk from Ives Hall).<br><br>
-
-Review the <a href="http://www.cornell.edu/about/maps/">campus map and directions to Ithaca, NY</a> to plan your trip and identify parking locations. We recommend parking in the Schoellkopf Field parking garage as it is free to visitors on Saturdays.<br><br>
-
-<strong>New York, NY- The Cornell Club, 6 East Ave 44th Street</strong><br>
-Check-in takes place between 9:00 – 9:45 a.m. in the Fall Creek Room. The program will run from 10:00 - 11:30 a.m.<br><br>
-
-Several parking lots are located within a couple blocks of the Club, such as Icon Parking and 200 Park Avenue garage.<br><br>
-
-<strong>Buffalo, NY- Buffalo and Erie County Public Library, 1 Lafayette Square</strong><br>
-Check-in takes place between 9:00 – 9:45 a.m. in the Central Library Meeting Room. The program will run from 10:00 - 11:30 a.m.<br><br>
-
-Several parking lots are located within a couple blocks of the Library, such as Fernbach Ramp and Adam Ramp garage.<br><br>
-
-Safe travels,<br><br>
-
-ILR Admissions Staff<br>
-Ilradmissions@cornell.edu<br>
-Office: 607.255.2222 Fax: 607.254.1251<br><br><br>
-
-
+Best,
+ILR Admissions Team<br><br><br>
 
 
 <strong>You submitted the following information with your Visit Days registration:</strong><br>
