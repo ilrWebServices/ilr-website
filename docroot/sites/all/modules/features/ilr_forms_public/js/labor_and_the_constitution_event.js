7b7c1c6686311da9611095c64d1613091deeb31d
David DeMello <daviddemello@gmail.com>
2019-09-12 15:22:54 -0400

    Removes early bird price and adds discount option to labor and the us constitution reg form.

--- docroot/sites/all/modules/features/ilr_forms_public/js/labor_and_the_constitution_event.js
+++ docroot/sites/all/modules/features/ilr_forms_public/js/labor_and_the_constitution_event.js
@@ -7,24 +7,40 @@
           set_price();
         });
 
-        function set_price() {
-          var currentPrice, priceNote;
-          var early_bird_deadline = new Date('9/13/2019');
-          var today = new Date();
+        function getUrlParameter(sParam) {
+            var sPageURL = window.location.search.substring(1),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
 
-          // Set full price based on date.
-          if ( (early_bird_deadline > today) ) {
-            currentPrice = 275;
-            priceNote = 'Registration fee ($275 until 9/13/19, $325 thereafter)'
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+                }
+            }
+        };
+
+        function set_price() {
+          var currentPrice, priceNote, studentPrice, studentPriceNote;
+
+          // Set full price (or discounted price if query string contains `discount=aj2KLm`).
+          if (getUrlParameter('discount') == 'aj2KLm') {
+            currentPrice = 150;
+            priceNote = 'Registration fee ($150 discounted rate)'
           } else {
-            currentPrice = 325;
-            priceNote = 'Registration fee ($325)'
+            currentPrice = 275;
+            priceNote = 'Registration fee ($275)'
           }
+
+          studentPrice = 25;
+          studentPriceNote = 'Registration fee ($25 student rate)';
 
           // Toggle full and discounted price
           if ( $('#edit-field-discount-eligible-und').is(':checked') ) {
-            $('#field-total-cost-add-more-wrapper input').val('25');
-            $('#field-total-cost-add-more-wrapper p').text('Registration fee ($25 student rate)');
+            $('#field-total-cost-add-more-wrapper input').val(studentPrice);
+            $('#field-total-cost-add-more-wrapper p').text(studentPriceNote);
           } else {
             $('#field-total-cost-add-more-wrapper input').val(currentPrice);
             $('#field-total-cost-add-more-wrapper p').text(priceNote);
