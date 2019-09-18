14cea41172a9c1108cda32a116178adb234848ed
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-09-18 15:29:12 -0400

    Update link to all news on homepage news block

--- docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--homepage-news.tpl.php
@@ -3,7 +3,7 @@
   <div class="cu-section-heading--framed cu-section-heading--framed-right cu-section-heading">
     <h2 class="cu-section-heading__heading">ILR News</h2>
     <a href="/news" class="cu-section-heading__link">
-      View All News
+      See all news
     </a>
   </div>
   <?php print render($content); ?>
