ee0e0d156059f123856be03d8f23aad084b3ad60
Joe Williams <jrw354@cornell.edu>
2019-10-31 14:50:47 -0400

    rebase

--- docroot/sites/all/themes/ilr_theme/js/wa.js
+++ docroot/sites/all/themes/ilr_theme/js/wa.js
@@ -4,11 +4,47 @@
     attach: function (context, settings) {
       var menuItems = ".menu li.menu-item";
       var menuLinks = ".menu-view-wrapper";
+      const isSubsite = $('body').hasClass('subsite');
 
+      const addKeyboardNav = function () {
+        if (isSubsite) {
+          return;
+        }
+
+        // Adding expander buttons after every top level nav item
+        $(menuItems).each(function (e) {
+          $(this).attr("aria-expanded", "false");
+          $(this).children('a').after("<button class='menuExpand' aria-expanded='false' tabindex='0' value='Expand Menu' aria-label='Expand " + $(this).children('a').text() + "'><span aria-hidden='true' class='fas fa-caret-down'></span></button>");
+        });
+
+        $('.menuExpand').on('touchstart click', function (e) {
+
+          var submenu = $(this).next('.submenu');
+
+          $(this).toggleClass('expanded');
+
+          if ($(this).hasClass('expanded')) {
+            $(this).attr('aria-expanded', 'true');
+          }
+          else {
+            $(this).attr('aria-expanded', 'false');
+          }
+
+          $('.menu-block-ilr-primary-menu').addClass('active');
+          $(submenu).toggleClass('active');
+
+          if ((submenu).hasClass('active')) {
+            $(submenu).css("opacity", "1");
+          }
+          else {
+            $(submenu).css("opacity", "0");
+          }
+        });
+      }
       // Adding expander buttons after every top level nav item
       $(menuItems).each(function(e){
         $(this).attr("aria-expanded","false");
-        $(this).children('a').after("<button class='menuExpand' aria-expanded='false' tabindex='0' value='Expand Menu' aria-label='Expand " + $(this).children('a').text() + "'><span aria-hidden='true' class='fas fa-caret-down'></span></button>");
+        $(this).children('a').after("<button class='menuExpand' aria-expanded='false' tabindex='0' value='Expand Menu'><span aria-label='Expand " + $(this).children('a').text() + "' class='fas fa-caret-down'></span></button>");
       });
 
       $('.menuExpand').on('touchstart click', function (e){
@@ -34,6 +70,7 @@
           $(submenu).css("opacity","0");
         }
       });
+>>>>>>> WA updates including mega menu and search updates
 
       // Able to close menu by hitting escape
       $(document).on('keydown', function(event) {
@@ -57,6 +94,14 @@
       $('.closeSearch').click(function(){
         $('header').toggleClass('search-engaged');
       });
+
+<<<<<<< HEAD
+      window.addEventListener('DOMContentLoaded', addKeyboardNav);
+=======
+      // $('.searchBtn').on('click', function(){
+      //   $('header').addClass('search-engaged');
+      // });
+>>>>>>> WA updates including mega menu and search updates
   }
 }
 
