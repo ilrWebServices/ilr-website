1f3d8eaf95d4d17a790ad20248a68489557cb5aa
Joe Williams <jrw354@cornell.edu>
2019-10-31 12:40:47 -0400

    updates to search to add button for wa

--- docroot/sites/all/themes/ilr_theme/templates/page.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/page.tpl.php
@@ -4,6 +4,10 @@
     <form action="/search" method="get" id="cu-search-form" accept-charset="UTF-8">
       <label class="sr-only" for="search-form-query">Search</label>
       <input id="search-form-query" type="text" name="s" placeholder="Search" value="" size="20" maxlength="128" class="form-text" tabindex="-1" />
+      <button type="submit" class="searchBtn" value="">
+        <span class="fas fa-search"></span>
+        <span class="sr-only">Search</span>
+      </button>
       <button type="button" class="closeSearch fas fa-times"><span class="sr-only">Close Search</span></button>
 
     </form>
@@ -13,8 +17,8 @@
     <?php if (isset($logo_override)): ?>
       <?php print $logo_override['#markup']; ?>
     <?php else: ?>
-    <div class="logo-wrapper"> 
-      <!-- <?php print $logo_link ?> -->
+    <div class="logo-wrapper">
+      <?php print $logo_link ?>
       <a class="ilr" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
          <img src="<?php echo $logo?>" alt="Cornell University | ILR School" title="Cornell University | ILR School">
       </a>
