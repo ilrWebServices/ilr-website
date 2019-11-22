2fd896929d9cf5223864504f239554cc63375a92
Aaron Froehlich <aaronfroehlich@gmail.com>
2019-11-22 09:29:21 -0500

    Revert "Remove title and img link from people profile teasers"
    This reverts commit 810d787344f46c561d89716d5a2406fc9d505e46.

--- docroot/sites/all/themes/ilr_theme/templates/field--field-first-name--people-profile.tpl.php
+++
@@ -1,6 +0,0 @@
-Read more about
-<?php foreach ($items as $delta => $item): ?>
-  <div class="<?php print $classes; ?>"<?php print $attributes; ?>>
-    <?php print render($item); ?>
-  </div>
-<?php endforeach; ?>
