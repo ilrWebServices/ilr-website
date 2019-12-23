f42961dfda2c93553da5a66cb123b22912df42aa
David DeMello <daviddemello@gmail.com>
2019-12-23 10:41:22 -0500

    Adds application form for Democracy Summer Fellowships in Buffalo.

---
+++ docroot/sites/all/modules/features/forms/templates/democracy-summer-fellowship-appl.tpl.php
@@ -0,0 +1,7 @@
+<?php print $salutation;?><br><br>
+
+This is a confirmation from the ILR School at Cornell University of your application for a Democracy Summer Fellowship for Summer 2020 in Buffalo, NY.<br><br>
+
+<?php print $form_data;?><br><br>
+
+If you have any questions, please contact Megan Connelly at mlc322@cornell.edu.
