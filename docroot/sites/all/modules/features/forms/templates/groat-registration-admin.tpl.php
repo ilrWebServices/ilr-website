064a3f4a6dee4a113c7f07b3d59f7d7c62143ae9
David DeMello <daviddemello@gmail.com>
2019-12-23 07:18:42 -0500

    Adds 2020 Groat registration form.

---
+++ docroot/sites/all/modules/features/forms/templates/groat-registration-admin.tpl.php
@@ -0,0 +1,11 @@
+The following Groat and Alpern 2020 registration has been submitted:<br><br>
+
+
+<strong>Registration summary (including gifts to ILR):</strong><br><br>
+<?php print $payment_gift_details; ?><br><br>
+
+<strong>Payment information sent to registering guest:</strong><br><br>
+<?php print $event_payment_details; ?><br><br>
+
+<strong>Complete information submitted with registration:</strong><br><br>
+<?php print $form_data; ?><br><br>
---
+++ docroot/sites/all/modules/features/forms/templates/groat-registration.tpl.php
@@ -0,0 +1,30 @@
+<?php print $salutation;?><br><br>
+
+Your reservation for the 2020 Groat and Alpern Awards Celebration has been received.<br>
+Event details and your registration information, including a statement of your gift amount, are shown below.<br>
+Please review this information and contact our office if you have any questions about your registration.<br><br>
+
+A portion of each registration will be a gift to the ILR School to support our student programs.<br>
+Please save this email; it serves as your gift receipt.<br><br>
+
+<strong>Event Details:</strong><br>
+Thursday, April 23, 2020<br>
+6:00 PM to 9:00 PM<br><br>
+
+Pierre Hotel, Grand Ballroom and Garden Foyer<br>
+2 East 61st Street at Fifth Avenue<br>
+New York City<br><br>
+
+<strong>Registration Details (including receipt of your gift to ILR):</strong><br><br>
+<?php print $payment_gift_details; ?><br><br>
+
+<strong>Payment Information:</strong><br><br>
+<?php print $event_payment_details; ?><br><br>
+
+Thank you for registering for the ILR Groat and Alpern Awards Celebration.<br>
+If you have any questions, please contact Sue Sappington, <a href="mailto:ss446@cornell.edu">ss446@cornell.edu</a>,<br>
+Office of Alumni Affairs and Development, ILR School, Cornell University.<br><br>
+
+
+<strong>Following is the information you submitted with your registration:</strong><br><br>
+<?php print $form_data; ?><br><br>
