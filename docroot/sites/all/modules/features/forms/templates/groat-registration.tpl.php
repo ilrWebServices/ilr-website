00023df325c817373c0ced5bd62aacc06ad1f0ef
David DeMello <daviddemello@gmail.com>
2020-01-16 16:18:23 -0500

    Adds support for delegated Groat/Aplern registrations for which no credit card is charged.

--- docroot/sites/all/modules/features/forms/templates/groat-registration.tpl.php
+++ docroot/sites/all/modules/features/forms/templates/groat-registration.tpl.php
@@ -1,11 +1,14 @@
 <?php print $salutation;?><br><br>
 
-Your reservation for the 2020 Groat and Alpern Awards Celebration has been received.<br>
-Event details and your registration information, including a statement of your gift amount, are shown below.<br>
-Please review this information and contact our office if you have any questions about your registration.<br><br>
+You have been registered for the 2020 Groat and Alpern Awards Celebration.<br><br>
 
-A portion of each registration will be a gift to the ILR School to support our student programs.<br>
-Please save this email; it serves as your gift receipt.<br><br>
+<?php if ($registrant_type == 'Alumni'): ?>
+  Event details and your registration information, including a statement of your gift amount, are shown below.<br>
+  Please review this information and contact our office if you have any questions about your registration.<br><br>
+
+  A portion of each registration will be a gift to the ILR School to support our student programs.<br>
+  Please save this email; it serves as your gift receipt.<br><br>
+<?php endif; ?>
 
 <strong>Event Details:</strong><br>
 Thursday, April 23, 2020<br>
@@ -15,16 +18,22 @@ Pierre Hotel, Grand Ballroom and Garden Foyer<br>
 2 East 61st Street at Fifth Avenue<br>
 New York City<br><br>
 
-<strong>Registration Details (including receipt of your gift to ILR):</strong><br><br>
-<?php print $payment_gift_details; ?><br><br>
+<?php if ($registrant_type == 'Alumni'): ?>
+  <strong>Registration Details (including receipt of your gift to ILR):</strong><br><br>
+  <?php print $payment_gift_details; ?><br><br>
 
-<strong>Payment Information:</strong><br><br>
-<?php print $event_payment_details; ?><br><br>
+  <strong>Payment Information:</strong><br><br>
+  <?php print $event_payment_details; ?><br><br>
 
-Thank you for registering for the ILR Groat and Alpern Awards Celebration.<br>
+  Thank you for registering for the ILR Groat and Alpern Awards Celebration.<br>
+<?php endif; ?>
+
+We look forward to seeing you there!<br><br>
+
 If you have any questions, please contact Sue Sappington, <a href="mailto:ss446@cornell.edu">ss446@cornell.edu</a>,<br>
 Office of Alumni Affairs and Development, ILR School, Cornell University.<br><br>
 
-
-<strong>Following is the information you submitted with your registration:</strong><br><br>
-<?php print $form_data; ?><br><br>
+<?php if ($registrant_type == 'Alumni'): ?>
+  <strong>Following is the information you submitted with your registration:</strong><br><br>
+  <?php print $form_data; ?><br><br>
+<?php endif; ?>
