4183652f6c5dd76f4587a5b6bfcd345211919d6b
Aaron Froehlich <aaronfroehlich@gmail.com>
2020-03-19 13:52:24 -0400

    Remove frame from ilr in the news page

--- docroot/sites/all/themes/ilr_theme/templates/block--bean--ilr-in-the-news-featured-stories.tpl.php
+++ docroot/sites/all/themes/ilr_theme/templates/block--bean--ilr-in-the-news-featured-stories.tpl.php
@@ -1,6 +1,4 @@
 <div class="featured-news <?php print $classes; ?>">
   <?php print render($title_suffix); ?>
-  <div class="cu-section-heading--framed cu-section-heading--framed-right cu-section-heading">
-  </div>
   <?php print render($content); ?>
 </div>
