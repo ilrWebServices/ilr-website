690f9ed88a40c3791f979afd8ac259b6aea9129c
Aaron Froehlich <aaronfroehlich@gmail.com>
2021-10-05 16:14:47 -0400

    Remove the now-defunct data pull and the scripts mount

--- bin/ilr_profiles_data_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This file to be uploaded to scripts folder on platform.sh
-# eg: scp ilr_profiles_data_pull.sh dd2imk5jkez6q-master-7rqtwti--app@ssh.us-2.platform.sh:/app/scripts/
-curl http://webutilities.ilr.cornell.edu/web/ilr_profiles_generate_data/
